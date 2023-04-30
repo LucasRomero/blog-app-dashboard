471f34821dc177665c83c9457f6488416d4873b1
LucasRomero <romerolucasdaniel@outlook.com>
2023-04-30 16:53:44 -0300

    function to create permalink, with regex font-size: 14px globally

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent {
+  permalink: string = '';
 
+  onTitleChanged(event: any): void {
+    const title = event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+  }
 }

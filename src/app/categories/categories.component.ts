df1d033e7f521eae448cc8880db7774d3d9cf891
LucasRomero <romerolucasdaniel@outlook.com>
2023-04-30 10:50:05 -0300

    update data

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../models/category';
 
@@ -7,14 +7,38 @@ import { Category } from '../models/category';
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categoryArray: Array<any> = [];
+  formCategory: string = '';
+  formStatus: string = 'Add';
+  categoryId: string = '';
+
   private categoriesService = inject(CategoriesService);
+
+  ngOnInit(): void {
+    this.categoriesService.loadData().subscribe((val) => {
+      this.categoryArray = val;
+    });
+  }
 
   onSubmit(formData: any) {
     let categoryData: Category = {
       category: formData.value.category,
     };
 
-    this.categoriesService.saveData(categoryData);
+    if (this.formCategory == 'Add') {
+      this.categoriesService.saveData(categoryData);
+    } else if (this.formCategory == 'Edit') {
+      this.categoriesService.updateData(this.categoryId, categoryData);
+    }
+
+    formData.reset();
+    this.formStatus = 'Add';
+  }
+
+  onEdit(category: any, id: string): void {
+    this.formCategory = category;
+    this.formStatus = 'Edit';
+    this.categoryId = id;
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, fromDocRef } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
 
 import { Category } from '../models/category';
+
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,13 +16,41 @@ export class CategoriesService {
 
   constructor() {}
 
-  saveData(data: Category) {
+  saveData(data: Category): void {
     this.afs
       .collection('categories')
       .add(data)
       .then((docRef) => {
         console.log(docRef);
         this.toastr.success('Data Insert Succesfully..!');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  loadData() {
+    return this.afs
+      .collection('categories')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  updateData(id: string, editData: any): void {
+    this.afs
+      .collection('categories')
+      .doc(id)
+      .update(editData)
+      .then((fromDocRef) => {
+        this.toastr.success('Data Updated Succesfully..!');
       })
       .catch((err) => {
         console.log(err);
